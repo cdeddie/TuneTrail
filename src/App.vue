7b39333c0424c5082a0a695aac1d1d86c1598c88
cdeddie <edwinchendesailly@gmail.com>
2024-04-03 03:19:15 +1030

    oops

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,22 @@
 <script setup>
 import Navbar from './components/Navbar.vue';
+import { ref, onMounted } from 'vue';
+
+const visible = ref(false);
+
+onMounted(() => {
+  visible.value = true;
+});
 </script>
 
 <template>
+  <Dialog v-model:visible="visible" modal header="Warning" :style="{ maxWidth: '600px', fontSize: '1rem' }">
+    <div style="text-align: center">
+      Please note that this website is currently in development. 
+      To offer you the best experience, I'm in the process of obtaining approval from Spotify for open usage of their API. 
+      Unfortunately, this means these services are not available for use at the moment. 
+    </div>
+  </Dialog>
   <div class="app-container">
     <Navbar />
 
